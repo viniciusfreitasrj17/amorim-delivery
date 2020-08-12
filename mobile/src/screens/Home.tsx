b5ec6e00b81b419bade028854c7f79f2c07c8dd2
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-08-12 00:14:16 -0300

    🐞 Error ScrollView Components FlatLists

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -67,7 +67,7 @@ const Home: React.FC<Props> = ({ navigation }) => {
     return (
       <View>
         <View style={{ marginHorizontal: 15 }}>
-          <TouchableOpacity>
+          <TouchableOpacity style={{ marginVertical: 8 }}>
             <Image
               source={{ uri: item.image }}
               style={{
@@ -84,25 +84,6 @@ const Home: React.FC<Props> = ({ navigation }) => {
     );
   };
 
-  const Promocao = ({ item }: { item: string }) => {
-    return (
-      <View>
-        <View style={{ marginHorizontal: 5 }}>
-          <Image
-            source={{ uri: item }}
-            style={{
-              borderRadius: 5,
-              height: 200,
-              width: widthScreen - 10,
-              borderWidth: 1,
-              borderColor: "black",
-            }}
-          />
-        </View>
-      </View>
-    );
-  };
-
   const [promo, setPromo] = useState([
     "http://tutofox.com/foodapp//banner/banner-1.jpg",
     "http://tutofox.com/foodapp//banner/banner-2.jpg",
@@ -111,63 +92,182 @@ const Home: React.FC<Props> = ({ navigation }) => {
 
   const [activeIndexPromo, setActiveIndexPromo] = useState(0);
 
+  const Promocao = ({ item }: { item: string }) => {
+    return (
+      <View>
+        <View style={{ marginHorizontal: 5 }}>
+          <TouchableOpacity style={{ marginVertical: 8 }}>
+            <Image
+              source={{ uri: item }}
+              style={{
+                borderRadius: 5,
+                height: 200,
+                width: widthScreen - 10,
+                borderWidth: 1,
+                borderColor: "black",
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  const [foods, setFoods] = useState([
+    {
+      id: "0",
+      name: "Calabresa",
+      description: "aaa, vcsc, fsdf, fdsmkf, fnsdl, fds ",
+      image:
+        "https://blog.novasafra.com.br/wp-content/uploads/2016/01/accb7d_37d37d9f1001e88384dafc6e5cc60c4f.jpg_1024-780x450.jpg",
+      stock: true,
+      price: "35",
+      category: {
+        id: "5803d9d7-177f-4566-815b-e5ce4b7ee86f",
+        name: "Pizza",
+      },
+    },
+    {
+      id: "1",
+      name: "De Banana, 500ml",
+      description: "aaa, vcsc, fsdf, fdsmkf, fnsdl, fds ",
+      image:
+        "https://t1.rg.ltmcdn.com/pt/images/9/8/9/vitamina_de_acai_com_leite_em_po_8989_paso_2_600.jpg",
+      stock: true,
+      price: "7",
+      category: {
+        id: "9eaf4cc6-1bb3-45e3-befa-77a79e432091",
+        name: "Açaí",
+      },
+    },
+    {
+      id: "2",
+      name: "X-Burguer",
+      description: "aaa, vcsc, fsdf, fdsmkf, fnsdl, fds ",
+      image: "https://zullalanches.com.br/wp-content/uploads/2019/06/9.jpg",
+      stock: true,
+      price: "10",
+      category: {
+        id: "dc74651c-6625-4fed-9c38-32a0a9f71a4a",
+        name: "Hambúrguer",
+      },
+    },
+  ]);
+
+  interface FoodInterface {
+    item: {
+      id: string;
+      name: string;
+      description: string;
+      image: string;
+      stock: boolean;
+      price: string;
+      category: {
+        id: string;
+        name: string;
+      };
+    };
+  } /*renderItem={({ item }) => <Text>{item.title}</Text>}*/
+
+  const Food: React.FC<FoodInterface> = ({ item }) => {
+    return (
+      <View>
+        <View style={{ borderWidth: 1, margin: 10, padding: 2 }}>
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginVertical: 8,
+            }}
+          >
+            <View style={{ flex: 1 }}>
+              <Text> {item.name} </Text>
+              <Text> {item.description} </Text>
+              <Text> {item.category.name} </Text>
+              <Text> {item.price} </Text>
+            </View>
+            <View>
+              <Image
+                source={{ uri: item.image }}
+                style={{
+                  borderRadius: 5,
+                  height: 150,
+                  width: 150,
+                  borderWidth: 1,
+                  borderColor: "black",
+                }}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Title title="Home" navigation={navigation} />
-      <ScrollView>
-        <Text style={stylesLocal.label}>Categorias</Text>
+      {/* <ScrollView> */}
+      <Text style={stylesLocal.label}>Categorias</Text>
 
-        <FlatList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          legacyImplementation={false}
-          data={categories}
-          keyExtractor={(item) => item.id}
-          renderItem={Category}
-          contentContainerStyle={{
-            justifyContent: "center",
-            flexDirection: "row",
-            paddingVertical: 15,
-          }}
-        />
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        legacyImplementation={false}
+        data={categories}
+        keyExtractor={(item) => item.id}
+        renderItem={Category}
+        contentContainerStyle={{
+          justifyContent: "center",
+          flexDirection: "row",
+          paddingVertical: 15,
+        }}
+      />
 
-        <Text style={stylesLocal.label}>Promoções</Text>
+      <Text style={stylesLocal.label}>Promoções</Text>
 
-        <Carousel
-          layout={"default"}
-          ref={(ref) => ref}
-          data={promo}
-          sliderWidth={widthScreen - 5}
-          itemWidth={widthScreen}
-          renderItem={Promocao}
-          onSnapToItem={(index) => setActiveIndexPromo(index)}
-          autoplay={true}
-          enableMomentum={false}
-          lockScrollWhileSnapping={false}
-          autoplayInterval={3000}
-          slideStyle={{
-            justifyContent: "center",
-            flexDirection: "row",
-            paddingVertical: 15,
-          }}
-        />
+      <Carousel
+        layout={"default"}
+        ref={(ref) => ref}
+        data={promo}
+        sliderWidth={widthScreen - 5}
+        itemWidth={widthScreen}
+        renderItem={Promocao}
+        onSnapToItem={(index) => setActiveIndexPromo(index)}
+        autoplay={true}
+        enableMomentum={false}
+        lockScrollWhileSnapping={false}
+        autoplayInterval={3000}
+        slideStyle={{
+          justifyContent: "center",
+          flexDirection: "row",
+          paddingVertical: 15,
+        }}
+      />
 
-        <Text style={stylesLocal.label}>Histórico</Text>
+      <Text style={stylesLocal.label}>Histórico</Text>
 
-        <FlatList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          legacyImplementation={false}
-          data={categories}
-          keyExtractor={(item) => item.id}
-          renderItem={Category}
-          contentContainerStyle={{
-            justifyContent: "center",
-            flexDirection: "row",
-            paddingVertical: 15,
-          }}
-        />
-      </ScrollView>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        legacyImplementation={false}
+        data={categories}
+        keyExtractor={(item) => item.id}
+        renderItem={Category}
+        contentContainerStyle={{
+          justifyContent: "center",
+          flexDirection: "row",
+          paddingVertical: 15,
+        }}
+      />
+
+      <Text style={stylesLocal.label}>Produtos</Text>
+      <FlatList
+        data={foods}
+        keyExtractor={(item) => item.id}
+        renderItem={Food}
+      />
+      {/* </ScrollView> */}
     </SafeAreaView>
   );
 };
--- mobile/src/styles/global.ts
+++ mobile/src/styles/global.ts
@@ -2,7 +2,7 @@ import { StyleSheet, Platform } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: Platform.OS === "android" ? 25 : 0,
-    marginBottom: Platform.OS === "android" ? 42 : 0,
   },
 });
